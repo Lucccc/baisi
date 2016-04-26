7d52aacaf264495d91b5364721ab481948ccbe08
Lei Gao <stray4520@163.com>
2016-04-26 22:27:10 +0800

    Initial Commit

---
+++ 百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by Lucccc on 16/4/26.
+//  Copyright © 2016年 Lucccc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
